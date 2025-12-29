64b014256ec3b6133bd868a87794581876d46ee1
Awaken <93601997+Awakenchan@users.noreply.github.com>
2025-12-30 04:41:23 +0500

    Update Build.lua

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -1,11 +1,13 @@
 return setmetatable({}, {
     __index = function(_, key)
         if key == "__version" then
-            return "1.0"
+            return "3.0"
         elseif key == "__log" then
             return {
-                "[+] Added isexecutorfunc check if it exist",
-                "[+] Added save function output"
+                "[+] Added proper actor system",
+                "[+] Re-factor entire script",
+                "[+] Actor frame is alot more stable now",
+                "[+] Rewrote filtering, rewrote start ups, rewrote script decompile"
             }
         end
         return nil 
