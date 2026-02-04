9e221d3920582609ddd5a9cb3dd22b97bf5f361a
Awaken <93601997+Awakenchan@users.noreply.github.com>
2026-02-04 05:11:23 +0500

    Update Build.lua

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -4,10 +4,7 @@ return setmetatable({}, {
             return "3.0"
         elseif key == "__log" then
             return {
-                "[+] Added proper actor system",
-                "[+] Re-factor entire script",
-                "[+] Actor frame is alot more stable now",
-                "[+] Rewrote filtering, rewrote start ups, rewrote script decompile"
+                "[-] fixed gc tables"
             }
         end
         return nil 
