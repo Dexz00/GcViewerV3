c4a871a890ff3fab1115a8d9920d3c58f2d49166
Awaken <93601997+Awakenchan@users.noreply.github.com>
2026-02-04 06:42:45 +0500

    Update Build.lua

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -1,10 +1,11 @@
 return setmetatable({}, {
     __index = function(_, key)
         if key == "__version" then
-            return "3.1"
+            return "3.1.2"
         elseif key == "__log" then
             return {
-                "[-] fixed gc tables"
+                "[-] fixed gc tables",
+                "[+] Added disassemble by -- by lovrewe"
             }
         end
         return nil 
